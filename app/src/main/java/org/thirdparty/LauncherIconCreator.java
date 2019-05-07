0c0064e2a3ed38de4eb27f14b7f5c80b038475a3
cunnj <johnc7995@gmail.com>
2019-05-06 21:23:25 -0400

    Added PinShortcut support, adaptive icon support and UI tweaks

--- app/src/main/java/org/thirdparty/LauncherIconCreator.java
+++ app/src/main/java/org/thirdparty/LauncherIconCreator.java
@@ -11,19 +11,30 @@
 
 package org.thirdparty;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ShortcutInfo;
+import android.content.pm.ShortcutManager;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.widget.Toast;
+
+import java.util.Objects;
 
 import com.cunnj.activities.MyActivityInfo;
 import com.cunnj.activities.R;
 
 public class LauncherIconCreator {
-    public static Intent getActivityIntent(ComponentName activity) {
+
+    private static Intent getActivityIntent(ComponentName activity) {
         Intent intent = new Intent();
         intent.setComponent(activity);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -37,44 +48,102 @@ public class LauncherIconCreator {
 
         // Use bitmap version if icon from different package is used
         if (!pack.equals(activity.getComponentName().getPackageName())) {
-            createLauncherIcon(context, activity.getComponentName(), activity.getName(), activity.getIcon());
+            createShortcut(context, activity.getName(), activity.getIcon(), getActivityIntent(activity.getComponentName()), null);
         } else {
-            createLauncherIcon(context, activity.getComponentName(), activity.getName(), activity.getIconResourceName());
+            createShortcut(context, activity.getName(), activity.getIcon(), getActivityIntent(activity.getComponentName()),
+                    activity.getIconResourceName());
         }
     }
 
-    public static void createLauncherIcon(Context context, ComponentName activity, String name, Drawable iconTemp) {
-        BitmapDrawable icon = (BitmapDrawable) iconTemp;
-        Toast.makeText(context, String.format(context.getText(R.string.creating_activity_shortcut).toString(), name), Toast.LENGTH_LONG).show();
+    /**
+     * Got reference from stackoverflow.com Url:
+     * https://stackoverflow.com/questions/44447056/convert-adaptiveicondrawable-to-bitmap-in-android-o-preview?utm_medium=organic&utm_source=
+     * google_rich_qa&utm_campaign=google_rich_qa
+     */
+    private static Bitmap getBitmapFromDrawable(Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
 
-        Intent shortcutIntent = new Intent();
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, getActivityIntent(activity));
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        Bitmap bm = icon.getBitmap();
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON, bm);
-        shortcutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        context.sendBroadcast(shortcutIntent);
-        //finish();
-    }
-
-    public static void createLauncherIcon(Context context, ComponentName activity, String name, String icon_resource_name) {
-        Toast.makeText(context, String.format(context.getText(R.string.creating_activity_shortcut).toString(), name), Toast.LENGTH_LONG).show();
-
-        Intent shortcutIntent = new Intent();
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, getActivityIntent(activity));
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name);
-        Intent.ShortcutIconResource ir = new Intent.ShortcutIconResource();
-        ir.packageName = activity.getPackageName();
-        ir.resourceName = icon_resource_name;
-        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, ir);
-        shortcutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
-        context.sendBroadcast(shortcutIntent);
-        //finish();
+        Bitmap bmp = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmp);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bmp;
     }
 
     public static void launchActivity(Context context, ComponentName activity, String name) {
         Intent intent = LauncherIconCreator.getActivityIntent(activity);
-        Toast.makeText(context, String.format(context.getText(R.string.starting_activity).toString(), name), Toast.LENGTH_LONG).show();
-        context.startActivity(intent);
+        Toast.makeText(context, String.format(context.getText(R.string.starting_activity).toString(), name),
+                Toast.LENGTH_LONG).show();
+        try {
+            context.startActivity(intent);
+        } catch (Exception e) {
+            Toast.makeText(context, context.getText(R.string.error_starting_activity).toString() + ": " + e.toString(), Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private static void createShortcut(Context context, String appName, Drawable draw, Intent intent, String iconResourceName) {
+        Toast.makeText(context, String.format(context.getText(R.string.creating_activity_shortcut).toString(), appName),
+                Toast.LENGTH_LONG).show();
+
+        if (Build.VERSION.SDK_INT >= 26) {
+            doCreateShortcut(context, appName, draw, intent);
+        } else {
+            doCreateShortcut(context, appName, intent, iconResourceName);
+        }
+    }
+
+    @TargetApi(14)
+    private static void doCreateShortcut(Context context, String appName, Intent intent, String iconResourceName) {
+        Intent shortcutIntent = new Intent();
+        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, intent);
+        shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_NAME, appName);
+        if (iconResourceName != null) {
+            Intent.ShortcutIconResource ir = new Intent.ShortcutIconResource();
+            if (intent.getComponent() == null) {
+                ir.packageName = intent.getPackage();
+            } else {
+                ir.packageName = intent.getComponent().getPackageName();
+            }
+            ir.resourceName = iconResourceName;
+            shortcutIntent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, ir);
+        }
+        shortcutIntent.setAction("com.android.launcher.action.INSTALL_SHORTCUT");
+        context.sendBroadcast(shortcutIntent);
+    }
+
+    @TargetApi(26)
+    private static void doCreateShortcut(Context context, String appName, Drawable draw, Intent intent) {
+        ShortcutManager shortcutManager = Objects.requireNonNull(context.getSystemService(ShortcutManager.class));
+
+        if (shortcutManager.isRequestPinShortcutSupported()) {
+            Bitmap bitmap = getBitmapFromDrawable(draw);
+            intent.setAction(Intent.ACTION_CREATE_SHORTCUT);
+
+
+            ShortcutInfo shortcutInfo = new ShortcutInfo.Builder(context, appName)
+                    .setShortLabel(appName)
+                    .setLongLabel(appName)
+                    .setIcon(Icon.createWithBitmap(bitmap))
+                    .setIntent(intent)
+                    .build();
+
+            shortcutManager.requestPinShortcut(shortcutInfo, null);
+        } else {
+            new AlertDialog.Builder(context)
+                    .setTitle(context.getText(R.string.error_creating_shortcut))
+                    .setMessage(context.getText(R.string.error_verbose_pin_shortcut))
+                    .setPositiveButton(context.getText(android.R.string.ok), new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // Just close dialog don't do anything
+                            dialog.cancel();
+                        }
+                    })
+                    .show();
+        }
     }
 }
